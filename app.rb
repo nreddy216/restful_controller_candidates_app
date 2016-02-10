20a2bd815ffc6c62fe26086d173254007bfe61c6
nreddy216 <nreddy216@gmail.com>
2016-02-09 23:49:06 -0800

    added images per candidate

--- app.rb
+++ app.rb
@@ -6,7 +6,15 @@ class RestfulControllerApp < Sinatra::Base
 
   get '/candidates/:id' do
     @candidates = ["Bernie Sanders", "Hillary Clinton", "Donald Trump", "Ted Cruz"]
-    @candidate = @candidates[params[:id].to_i - 1]
+    @candidate = @candidates[params[:id].to_i - 1].split(' ')[0]
+    
+    erb :'index'
+  end
+
+
+
+
+  get '/candidates/:id' do
     erb :'index'
   end
 
---
+++ public/README.md
@@ -0,0 +1,8 @@
+#Candidates Web App
+
+02/09/2016
+
+This is a simple app that (currently) cycles through the names of the top presidential candidates.
+
+##Technologies used:
+* Ruby, using Sinatra
